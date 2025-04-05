a8b3e33b1c14944777fed1f086f2b905d645dfe9
Glen Sargent <glensarg@hotmail.co.uk>
2025-04-05 12:33:52 +0100

    chore: wasm

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+import GHC.Wasm.Prim
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "Hello from Haskell"
+
+
+foreign import javascript unsafe "console.log($1)"
+  js_print :: JSString -> IO ()
+
+foreign import javascript unsafe "typeof $1 === 'object'"
+  js_is_obj :: JSVal -> Bool
+
+foreign import javascript unsafe "let acc = 1; for (let i = 1; i <= $1; ++i) acc *= i; return acc;"
+  js_fac :: Word -> Word
